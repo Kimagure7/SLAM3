a76b4732e0d3e1e0f06cc0daceb1c8ed8d50063a
ZoeyChiu-ry4090 <2477036173@qq.com>
2024-07-17 09:27:05 +0800

    进一步删除了部分编译选项

--- Examples/Monocular-Inertial/mono_inertial_gopro_vi.cc
+++ Examples/Monocular-Inertial/mono_inertial_gopro_vi.cc
@@ -112,24 +112,14 @@ int main(int argc, char **argv) {
       }
 
 
-#ifdef COMPILEDWITHC14
       std::chrono::steady_clock::time_point t1 =
           std::chrono::steady_clock::now();
-#else
-      std::chrono::monotonic_clock::time_point t1 =
-          std::chrono::monotonic_clock::now();
-#endif
 
       // Pass the image to the SLAM system
       SLAM.TrackMonocular(im_track, tframe, vImuMeas);
 
-#ifdef COMPILEDWITHC14
       std::chrono::steady_clock::time_point t2 =
           std::chrono::steady_clock::now();
-#else
-      std::chrono::monotonic_clock::time_point t2 =
-          std::chrono::monotonic_clock::now();
-#endif
 
       double ttrack =
           std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1)
