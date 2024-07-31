d6e938260ecb69453881f4ce99b0aadfb96d13ef
ZoeyChiu-ry4090 <2477036173@qq.com>
2024-07-31 15:16:33 +0800

    format

--- include/Viewer.h
+++ include/Viewer.h
@@ -1,20 +1,20 @@
 /**
-* This file is part of ORB-SLAM3
-*
-* Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
-* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
-*
-* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
-* License as published by the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
-* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
-* If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of ORB-SLAM3
+ *
+ * Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+ * Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+ *
+ * ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 #ifndef VIEWER_H
@@ -22,14 +22,13 @@
 
 #include "FrameDrawer.h"
 #include "MapDrawer.h"
-#include "Tracking.h"
-#include "System.h"
 #include "Settings.h"
+#include "System.h"
+#include "Tracking.h"
 
 #include <mutex>
 
-namespace ORB_SLAM3
-{
+namespace ORB_SLAM3 {
 
 class Tracking;
 class FrameDrawer;
@@ -37,13 +36,12 @@ class MapDrawer;
 class System;
 class Settings;
 
-class Viewer
-{
+class Viewer {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-    Viewer(System* pSystem, FrameDrawer* pFrameDrawer, MapDrawer* pMapDrawer, Tracking *pTracking, const string &strSettingPath, Settings* settings);
+    Viewer(System *pSystem, FrameDrawer *pFrameDrawer, MapDrawer *pMapDrawer, Tracking *pTracking, const string &strSettingPath, Settings *settings);
 
-    void newParameterLoader(Settings* settings);
+    void newParameterLoader(Settings *settings);
 
     // Main thread function. Draw points, keyframes, the current camera pose and the last processed
     // frame. Drawing is refreshed according to the camera fps. We use Pangolin.
@@ -61,19 +59,19 @@ public:
 
     void Release();
 
-    //void SetTrackingPause();
+    // void SetTrackingPause();
 
     bool both;
-private:
 
+private:
     bool ParseViewerParamFile(cv::FileStorage &fSettings);
 
     bool Stop();
 
-    System* mpSystem;
-    FrameDrawer* mpFrameDrawer;
-    MapDrawer* mpMapDrawer;
-    Tracking* mpTracker;
+    System *mpSystem;
+    FrameDrawer *mpFrameDrawer;
+    MapDrawer *mpMapDrawer;
+    Tracking *mpTracker;
 
     // 1/fps in ms
     double mT;
@@ -93,12 +91,9 @@ private:
     std::mutex mMutexStop;
 
     bool mbStopTrack;
-
 };
 
-}
+}    // namespace ORB_SLAM3
 
 
-#endif // VIEWER_H
-	
-
+#endif    // VIEWER_H
